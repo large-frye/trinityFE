2f12418e82d3b41d9b75b8f7519ff9dd417e774f
Andrew Frye <a.frye4@gmail.com>
2016-05-17 20:21:23 -0400

    adding server url

--- src/shared/services.js
+++ src/shared/services.js
@@ -387,7 +387,7 @@
                         // return 'http://52.2.169.5';
                         return 'http://api.trinity.dev';
                     }
-                    return 'http://api.trinity.is:4444';
+                    return 'http://52.2.169.5';
                 }
             }
         }]);
